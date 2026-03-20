4bea379bfdee386682ba089e259b890b60a66876
bhanuprasad121 <bhanuprasadpujari2000@gmail.com>
2026-03-20 20:12:41 +0530

    question

---
+++ 36.py
@@ -0,0 +1,31 @@
+# Write a Python Program to Find LCM.
+# Least Common Multiple (LCM):
+# LCM, or Least Common Multiple, is the smallest multiple that is exactly divisible by two or
+# more numbers.
+# Formula:
+# For two numbers a and b, the LCM can be found using the formula:
+
+# LCM(a, b) =
+
+# |a ⋅ b|
+# GCD(a, b)
+
+# In [1]:
+# For more than two numbers, you can find the LCM step by step, taking the LCM of pairs of
+# numbers at a time until you reach the last pair.
+
+def compute_lcm(x,y):
+    if x>y:
+        greater=x
+    else:
+        greater=y
+    while(True):
+        if ((greater%x==0)and(greater%y==0)):
+            lcm=greater
+            break
+        greater+=1
+    return lcm
+
+num1=int(input('Enter a number'))
+num2=int(input('Enter a number'))
+print('The Lcm of',compute_lcm(num1,num2))
